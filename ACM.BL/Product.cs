1573cb8ae13595ff34b3aa1bb47fa3c08a7bbb9f
Mark Huntjens <mark_huntjens@reyrey.com>
2018-10-17 09:29:31 +0200

    AdProductClass

---
+++ ACM.BL/Product.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    class Product
+    {
+        public Product()
+        {
+
+        }
+
+        public Product(int productId)
+        {
+            this.ProduktId = productId;
+        }
+        public Decimal? CurrentPrice { get; set; }
+        public int ProduktId { get; private set; }
+        public string ProductDescription { get; set; }
+        public string ProductName { get; set; }
+
+        /// <summary>
+        /// Retrieve one product
+        /// </summary>
+        /// <param name="productId"></param>
+        /// <returns></returns>
+        public Product Retrieve(int productId)
+        {
+            // code that retrieves one product
+            return new Product();
+        }
+
+        public bool Save()
+        {
+            // code that saves the product
+            return true;
+        }
+
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (string.IsNullOrWhiteSpace(ProductName)) isValid = false;
+            if (CurrentPrice == null) isValid = false;
+            return isValid;
+        }
+    }
+}
