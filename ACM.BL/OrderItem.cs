fc6c4883c280d68a8ea42e8e31eb8b0d6e3fcd1e
Mark <Mark@DESKTOP-16DN5B4>
2018-10-17 21:09:04 +0200

    AddOrderItem

---
+++ ACM.BL/OrderItem.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class OrderItem
+    {
+        public OrderItem()
+        {
+
+        }
+        public OrderItem(int orderItemId)
+        {
+            this.OrderItemId = orderItemId;
+        }
+
+        public int OrderItemId { get; private set; }
+        public int OrderQuantity { get; set; }
+        public int ProductId { get; set; }
+        public decimal? PurchasePrice { get; set; }
+
+
+        /// <summary>
+        /// Retrieve one order item
+        /// </summary>
+        /// <param name="orderItemId"></param>
+        /// <returns></returns>
+        public OrderItem Retrieve(int orderItemId)
+        {
+            // code that retrieves the defined order item
+            return new OrderItem();
+        }
+
+        /// <summary>
+        /// Saves the current order item
+        /// </summary>
+        /// <returns></returns>
+        public bool Save()
+        {
+            // code that saves current order item
+            return true;
+        }
+
+        /// <summary>
+        /// Validates the order item data
+        /// </summary>
+        /// <returns></returns>
+        public bool Validate()
+        {
+            var isValid = true;
+
+            if (OrderQuantity <= 0) isValid = false;
+            if (ProductId <= 0) isValid = false;
+            if (PurchasePrice == null) isValid = false;
+
+            return isValid;
+        }
+    }
+}
