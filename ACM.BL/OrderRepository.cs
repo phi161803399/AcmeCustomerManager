f7658b9c45a2e0c6316a615306bbf73a04da3fea
Mark <Mark@DESKTOP-16DN5B4>
2018-10-24 21:21:19 +0200

    AddOrderRepositoryTest

--- ACM.BL/OrderRepository.cs
+++ ACM.BL/OrderRepository.cs
@@ -23,6 +23,51 @@ namespace ACM.BL
             return order;
         }
 
+        public OrderDisplay RetrieveOrderDisplay(int orderId)
+        {
+            OrderDisplay orderDisplay = new OrderDisplay();
+            // Code that retrieves the defined order fields
+            // Temporary Hard-coded data
+            if (orderId == 10)
+            {
+                orderDisplay.FirstName = "Bilbo";
+                orderDisplay.LastName = "Baggins";
+                orderDisplay.OrderDate = new DateTimeOffset(2018, 10, 19, 10, 00, 00, new TimeSpan(2, 0, 0));
+                orderDisplay.ShippingAddress = new Address()
+                {
+                    AddressType = 1,
+                    StreetLine1 = "Bag End",
+                    StreetLine2 = "Bagshot row",
+                    City = "Hobbiton",
+                    State = "Shire",
+                    Country = "Middle Earth",
+                    PostalCode = "144"
+                };
+            }
+
+            orderDisplay.orderDisplayItemList = new List<OrderDisplayItem>();
+            // Code that retrieves the order items
+            // Temporary Hard-coded data
+            if (orderId == 10)
+            {
+                var orderDisplayItem = new OrderDisplayItem()
+                {
+                    ProductName = "Sunflowers",
+                    PurchasePrice = 15.96M,
+                    OrderQuantity = 2
+                };
+                orderDisplay.orderDisplayItemList.Add(orderDisplayItem);
+                orderDisplayItem = new OrderDisplayItem()
+                {
+                    ProductName = "Rake",
+                    PurchasePrice = 6M,
+                    OrderQuantity = 1
+                };
+                orderDisplay.orderDisplayItemList.Add(orderDisplayItem);
+            }
+            return orderDisplay;
+        }
+
         public bool Save()
         {
             // code that saves current order
--- ACM.BLTest/OrderRepositoryTest.cs
+++ ACM.BLTest/OrderRepositoryTest.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ACM.BL;
+using System.Collections.Generic;
 
 namespace ACM.BLTest
 {
@@ -23,6 +24,51 @@ namespace ACM.BLTest
             // Arrange
             Assert.AreEqual(expected.OrderDate, actual.OrderDate);
         
+        }
+        [TestMethod]
+        public void RetrieveOrderDisplayTest()
+        {
+            // Arrange
+            var orderRepository = new OrderRepository();
+            var expected = new OrderDisplay()
+            {
+                FirstName = "Bilbo",
+                LastName = "Baggins",
+                OrderDate = new DateTimeOffset(2018, 10, 19, 10, 00, 00, new TimeSpan(2, 0, 0)),
+                ShippingAddress = new Address()
+                {
+                    AddressType = 1,
+                    StreetLine1 = "Bag End",
+                    StreetLine2 = "Bagshot row",
+                    City = "Hobbiton",
+                    State = "Shire",
+                    Country = "Middle Earth",
+                    PostalCode = "144"
+                },
+                orderDisplayItemList = new List<OrderDisplayItem>()
+                {
+                    new OrderDisplayItem()
+                    {
+                        ProductName = "Sunflowers",
+                        PurchasePrice = 15.96M,
+                        OrderQuantity = 2
+                    },
+                    new OrderDisplayItem()
+                    {
+                        ProductName = "Rake",
+                        PurchasePrice = 6M,
+                        OrderQuantity = 1
+                    }
+                }
+            };
+
+            // Act
+            var actual = orderRepository.RetrieveOrderDisplay(10);
+
+            // Assert
+            Assert.AreEqual(expected.FirstName, actual.FirstName);
+            Assert.AreEqual(expected.LastName, actual.LastName);
+            // etc etc etc...
         }
     }
 }
